ff8c8cc23c0f1ad553f67cc38d1cd28b90787c18
Swinston07 <136063003+Swinston07@users.noreply.github.com>
2023-09-06 21:41:43 -0700

    Add files via upload

---
+++ HelloWorld17ALab/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Sterling Winston
+ *
+ * Created on February 15, 2023, 2:38 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global COnstants - Math/Physics/Chemistry/Conversions Only
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    
+    //Set Random number seed
+    
+    //Declare Variables
+    
+    //Initialize inputs
+    
+    //Map inputs to Outputs - Process
+    
+    //Display output
+    cout << "Hello World" << endl;
+    
+    //Exit stage right
+    return 0;
+}
